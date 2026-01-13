a196eafff629ed746eca8e80f4d57ef01321c7d5
Renato Aranda <renato.c.aranda@gmail.com>
2026-01-13 00:43:47 +0000

    finish api/v1/status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ status: "tudo certo aí" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = await database.query("SHOW server_version;");
+  const databaseMaxConnections = await database.query("SHOW max_connections;");
+  const databaseName = process.env.POSTGRES_DB;
+
+  const databaseOpenedConnections = await database.query({
+    text: "SELECT COUNT(*) as opened_connections FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    update_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: parseInt(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        opened_connections: parseInt(
+          databaseOpenedConnections.rows[0].opened_connections,
+        ),
+      },
+    },
+  });
 }
 
 export default status;
